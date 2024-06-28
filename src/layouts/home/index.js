7fdb3269f3c9d2101212b35ae030c9f0cb0bfeb1
손철훈 <sch9208@gmail.com>
2024-06-28 14:04:03 +0900

    메인화면 통계

--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -34,9 +34,35 @@ import { serverCommunicationUtil, sessionChecker } from "../../common/util/serve
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const { sales, tasks } = reportsLineChartData;
   const [showPage, setShowPage] = useState(false);
+  const [statistics, setStatistics] = useState([]);
   const navigate = useNavigate();
+
+  const getStatistics = () => {
+    serverCommunicationUtil("main", "axioPost", "/homeStatistics", {})
+      .then((result) => {
+        let conversionData = [];
+
+        for (var i = 0; i < result.length; i++) {
+          let dataList = [];
+          let labels = Object.keys(result[i].data);
+
+          for (var j = 0; j < labels.length; j++) {
+            dataList.push(parseInt(result[i].data[labels[j]]));
+          }
+
+          conversionData.push({
+            labels: labels,
+            datasets: { label: result[i].DBName, data: dataList },
+          });
+        }
+
+        setStatistics(conversionData);
+      })
+      .catch((error) => {
+        console.log("");
+      });
+  };
 
   useEffect(() => {
     sessionChecker().then((checkerResult) => {
@@ -46,6 +72,7 @@ function Home() {
         navigate("/login");
       }
     });
+    getStatistics();
   }, []);
 
   if (!showPage) {
@@ -80,155 +107,20 @@ function Home() {
         <meta name="description" content="DataMon2 Home" />
       </Helmet>
       <DashboardNavbar />
-      <MDBox py={3}>
-        <Grid container>
-          <SalesByCountry />
-        </Grid>
-        <MDBox mt={6}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsBarChart
-                  color="info"
-                  title="website views"
-                  description="Last Campaign Performance"
-                  date="campaign sent 2 days ago"
-                  chart={reportsBarChartData}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
-                  color="success"
-                  title="daily sales"
-                  description={
-                    <>
-                      (<strong>+15%</strong>) increase in today sales.
-                    </>
-                  }
-                  date="updated 4 min ago"
-                  chart={sales}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
-                  color="dark"
-                  title="completed tasks"
-                  description="Last Campaign Performance"
-                  date="just updated"
-                  chart={tasks}
-                />
-              </MDBox>
-            </Grid>
-          </Grid>
+      {statistics.map((statistic) => (
+        <MDBox py={3} key={statistic.datasets.label}>
+          <MDBox mt={6}>
+            <MDBox mb={3}>
+              <ReportsLineChart
+                color="info"
+                title={`${statistic.datasets.label} 일일 고객 데이터 수집량`}
+                date={`${new Date()}`}
+                chart={statistic}
+              />
+            </MDBox>
+          </MDBox>
         </MDBox>
-        <MDBox mt={1.5}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  color="dark"
-                  icon="weekend"
-                  title="Bookings"
-                  count={281}
-                  percentage={{
-                    color: "success",
-                    amount: "+55%",
-                    label: "than lask week",
-                  }}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  icon="leaderboard"
-                  title="Today's Users"
-                  count="2,300"
-                  percentage={{
-                    color: "success",
-                    amount: "+3%",
-                    label: "than last month",
-                  }}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  color="success"
-                  icon="store"
-                  title="Revenue"
-                  count="34k"
-                  percentage={{
-                    color: "success",
-                    amount: "+1%",
-                    label: "than yesterday",
-                  }}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  color="primary"
-                  icon="person_add"
-                  title="Followers"
-                  count="+91"
-                  percentage={{
-                    color: "success",
-                    amount: "",
-                    label: "Just updated",
-                  }}
-                />
-              </MDBox>
-            </Grid>
-          </Grid>
-        </MDBox>
-        <MDBox mt={2}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mt={3}>
-                <BookingCard
-                  image={booking1}
-                  title="Cozy 5 Stars Apartment"
-                  description='The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.'
-                  price="$899/night"
-                  location="Barcelona, Spain"
-                  action={actionButtons}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mt={3}>
-                <BookingCard
-                  image={booking2}
-                  title="Office Studio"
-                  description='The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.'
-                  price="$1.119/night"
-                  location="London, UK"
-                  action={actionButtons}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mt={3}>
-                <BookingCard
-                  image={booking3}
-                  title="Beautiful Castle"
-                  description='The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.'
-                  price="$459/night"
-                  location="Milan, Italy"
-                  action={actionButtons}
-                />
-              </MDBox>
-            </Grid>
-          </Grid>
-        </MDBox>
-      </MDBox>
+      ))}
       <Footer />
     </DashboardLayout>
   );
